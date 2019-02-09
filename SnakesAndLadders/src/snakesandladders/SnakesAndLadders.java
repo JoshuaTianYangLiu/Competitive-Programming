e049d1ca96e8136f49904c0fdedb6d31c2b1ba00
JoshuaTianYangLiu <46253730+JoshuaTianYangLiu@users.noreply.github.com>
2019-02-08 21:01:19 -0500

    Snake&Ladders

---
+++ SnakesAndLadders/src/snakesandladders/SnakesAndLadders.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package snakesandladders;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Joshua
+ */
+public class SnakesAndLadders {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner in = new Scanner(System.in);
+        int a = 1;
+        int read=1;
+        while(read!=0 && a<100){
+            read = in.nextInt();
+            if(read!=0){
+                a+=read;
+                if(a==54) a= 19;
+                else if(a==90) a= 48;
+                else if(a==99) a= 77;
+                else if(a==9) a= 34;
+                else if(a==40) a= 64;
+                else if(a== 67) a= 86; 
+                System.out.println("You are now on square "+a);
+            }
+        }
+        if(a>=100) System.out.println("You Win!");
+        else System.out.println("You Quit!");
+    }
+    
+}
