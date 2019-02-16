f92a0a21727fa033821550ca80ae92feabddb94c
JoshuaTianYangLiu <46253730+JoshuaTianYangLiu@users.noreply.github.com>
2019-02-16 16:45:19 -0500

    Added template

--- HappyFaceProcessing/src/happyfaceprocessing/HappyFaceProcessing.java
+++ HappyFaceProcessing/src/happyfaceprocessing/HappyFaceProcessing.java
@@ -25,56 +25,59 @@ public class HappyFaceProcessing {
     public static void main(String[] args) throws Exception{
         // TODO code application logic here
         List<int[]> s = new ArrayList<int[]>();
-        s.add(new int[] {20,10,20});
-        s.add(new int[] {16,4,10,4,16});
-        s.add(new int[] {14,2,18,2,14});
-        s.add(new int[] {12,2,22,2,12});
-        s.add(new int[] {11,1,26,1,11});
-        s.add(new int[] {9,2,28,2,9});
-        s.add(new int[] {8,1,32,1,8});
-        s.add(new int[] {7,1,34,1,7});
-        s.add(new int[] {6,1,36,1,6});
-        s.add(new int[] {5,1,38,1,5});
-        s.add(new int[] {5,1,38,1,5});
-        s.add(new int[] {4,1,40,1,4});
-        s.add(new int[] {3,1,42,1,3});
-        s.add(new int[] {2,2,8,4,18,4,8,2,2});
-        s.add(new int[] {2,1,7,2,4,2,14,2,4,2,7,1,2});
-        s.add(new int[] {2,2,6,1,6,1,14,1,6,1,6,2,2});
-        s.add(new int[] {1,1,2,1,4,1,8,1,12,1,8,1,4,1,2,1,1});
-        s.add(new int[] {1,1,3,5,3,2,3,14,3,2,3,5,3,1,1});
-        s.add(new int[] {1,1,7,1,3,2,3,1,12,1,3,2,3,1,7,1,1});
-        s.add(new int[] {1,1,7,1,8,1,12,1,8,1,7,1,1});
-        s.add(new int[] {0,1,9,1,6,1,14,1,6,1,9,1});
-        s.add(new int[] {0,1,9,2,4,2,14,2,4,2,9,1});
-        s.add(new int[] {0,1,11,4,18,4,11,1,0});
-        s.add(new int[] {0,1,48,1,0});
-        s.add(new int[] {0,1,24,1,23,1,0});
-        s.add(new int[] {0,1,24,1,23,1,0});
-        s.add(new int[] {0,1,24,1,23,1,0});
-        s.add(new int[] {0,1,23,1,24,1,0});
-        s.add(new int[] {0,1,22,1,25,1,0});
-        s.add(new int[] {1,1,46,1,1});
-        s.add(new int[] {1,1,46,1,1});
-        s.add(new int[] {1,1,46,1,1});
-        s.add(new int[] {1,1,46,1,1});
-        s.add(new int[] {2,1,44,1,2});
-        s.add(new int[] {2,1,44,1,2});
-        s.add(new int[] {3,1,9,2,20,2,9,1,3});
-        s.add(new int[] {3,1,9,4,16,4,9,1,3});
-        s.add(new int[] {4,1,9,5,12,5,9,1,4});
-        s.add(new int[] {5,1,9,6,8,6,9,1,5});
-        s.add(new int[] {5,1,10,18,10,1,5});
-        s.add(new int[] {6,1,11,14,11,1,6});
-        s.add(new int[] {7,1,12,10,12,1,7});
-        s.add(new int[] {8,1,32,1,8});
-        s.add(new int[] {9,2,28,2,9});
-        s.add(new int[] {11,1,26,1,11});
-        s.add(new int[] {12,2,22,2,12});
-        s.add(new int[] {14,2,18,2,14});
-        s.add(new int[] {16,4,10,4,16});
-        s.add(new int[] {20,10,20});
-        for(int i=0; i<49; i++){
+        s.add(new int[] {50});
+        s.add(new int[] {11,4,35});
+        s.add(new int[] {10,1,4,4,31});
+        s.add(new int[] {10,1,8,4,27});
+        s.add(new int[] {10,1,12,4,23});
+        s.add(new int[] {10,1,15,5,19});
+        s.add(new int[] {9,1,16,1,3,5,15});
+        s.add(new int[] {9,1,16,1,3,1,4,4,11});
+        s.add(new int[] {9,1,16,1,3,1,8,4,7});
+        s.add(new int[] {9,1,15,1,4,1,12,4,3});
+        s.add(new int[] {8,1,7,2,7,1,3,1,17,1,2});
+        s.add(new int[] {8,1,6,1,2,1,6,1,3,1,17,1,2});
+        s.add(new int[] {8,1,6,1,3,1,6,3,18,1,2});
+        s.add(new int[] {8,1,7,1,2,1,27,1,2});
+        s.add(new int[] {7,1,9,2,27,1,3});
+        s.add(new int[] {7,1,38,1,3});
+        s.add(new int[] {7,5,24,2,8,1,3});
+        s.add(new int[] {7,1,4,4,19,1,2,1,7,1,3});
+        s.add(new int[] {6,1,9,4,15,1,3,1,5,1,4});
+        s.add(new int[] {6,1,13,4,12,1,2,1,5,1,4});
+        s.add(new int[] {6,1,17,4,9,2,6,1,4});
+        s.add(new int[] {6,1,21,4,13,1,4});
+        s.add(new int[] {5,1,6,2,18,4,8,1,5});
+        s.add(new int[] {5,1,5,1,2,1,21,4,4,1,5});
+        s.add(new int[] {5,1,5,1,3,1,24,5,5});
+        s.add(new int[] {5,1,6,1,2,1,6,2,20,1,5});
+        s.add(new int[] {4,1,8,2,6,1,2,1,18,1,6});
+        s.add(new int[] {4,1,16,1,3,1,7,2,8,1,6});
+        s.add(new int[] {4,1,17,1,2,1,6,1,2,1,7,1,6});
+        s.add(new int[] {4,1,18,2,7,1,3,1,6,1,6});
+        s.add(new int[] {5,4,24,1,2,1,5,1,7});
+        s.add(new int[] {9,4,21,2,6,1,7});
+        s.add(new int[] {13,4,25,1,7});
+        s.add(new int[] {16,5,21,1,7});
+        s.add(new int[] {16,1,4,4,16,1,8});
+        s.add(new int[] {14,1,1,1,8,4,12,1,8});
+        s.add(new int[] {15,1,4,2,4,1,2,4,8,1,8});
+        s.add(new int[] {15,1,3,1,2,1,3,1,6,4,4,1,8});
+        s.add(new int[] {16,1,2,1,3,1,2,2,9,4,9});
+        s.add(new int[] {5,1,10,1,3,1,2,1,2,1,23});
+        s.add(new int[] {5,1,11,1,3,2,2,1,24});
+        s.add(new int[] {4,1,3,1,8,1,6,1,25});
+        s.add(new int[] {7,1,10,1,4,1,26});
+        s.add(new int[] {0,3,3,1,11,1,3,1,27});
+        s.add(new int[] {3,1,5,1,8,1,2,1,28});
+        s.add(new int[] {4,1,3,1,10,2,29});
+        s.add(new int[] {5,1,13,1,30});
+        s.add(new int[] {6,1,43});
+        s.add(new int[] {6,1,43});
+        s.add(new int[] {6,1,43});
+        
+        
+        for(int i=0; i<50; i++){
             String a = "";
             boolean b = true;
             for(int j=0; j<s.get(i).length; j++){
@@ -87,7 +90,7 @@ public class HappyFaceProcessing {
                 }
                 b=!b;
             }
-            System.out.println("println(\""+a+"\")" );
+            System.out.println("println(\""+a+"\");" );
         }
     }
 
