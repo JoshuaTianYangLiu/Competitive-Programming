43199dd4153a8c8c783fb9a9c5f50c94d832c977
JoshuaTianYangLiu <46253730+JoshuaTianYangLiu@users.noreply.github.com>
2019-10-31 00:00:07 -0400

    Bug and Clean old Java folders

---
+++ DMOJ&WLMOJ Java/updateFiles.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+for D in *; do
+	cd "$D";
+	if [ -d src ]; then
+		if [ -f src/**/*.java ]; then
+		mv src/**/*.java $D.java;
+		fi
+		find . -type f ! -name '*.java' -delete
+		find . -type d -delete
+		echo "Updated Folder $D, Currect files $(ls)";
+	else
+		if [ ! -f *.java ]; then
+		echo "				Unable to find files in $D";
+		fi
+	fi
+	cd ..;
+done
+
